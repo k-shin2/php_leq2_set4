def5b5caa22161f266b96c4aeaff2b45746f976f
k-shin2 <raptor.f22.us.airforce@gmail.com>
2019-01-29 09:56:09 +0900

    PHP学習で作成した入力内容を表示するフォーム

---
+++ userform_2.php
@@ -0,0 +1,14 @@
+
+<html>
+
+<head>
+<?php require_once('Section/title.php'); ?>
+<!-- <?php //include('Section/title.php'); ?> -->
+</head>
+
+<body>
+
+<?php require_once('Section/form1.php'); ?>
+<!-- <?php //include('Section/form1.php'); ?>-->
+</body>
+</html>
